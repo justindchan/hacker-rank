aa171bc7fd1b156da4f5b352049086a993b77ca1
Justin <justindchan@gmail.com>
2021-02-03 19:42:44 -0800

    solved hackerrank challenge day 2: operators.  Wrote function that computes totalBill, given meal_cost, tip_percent, and tax_percent.

---
+++ hacker-rank-challenge-day-2-OPERATORS.js
@@ -0,0 +1,55 @@
+// Task
+// Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip), and tax percent (the percentage of the meal price being added as tax) for a meal, find and print the meal's total cost. Round the result to the nearest integer.
+
+// Sample Input
+
+// 12.00
+// 20
+// 8
+
+// Sample Output
+
+// 15
+
+// code
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the solve function below.
+function solve(meal_cost, tip_percent, tax_percent) {
+    let tipAmount = meal_cost * (tip_percent * .01);
+    let taxAmount = meal_cost * (tax_percent * .01);
+    let totalBill = meal_cost + tipAmount + taxAmount;
+    console.log(Math.round(totalBill));
+}
+
+function main() {
+    const meal_cost = parseFloat(readLine());
+
+    const tip_percent = parseInt(readLine(), 10);
+
+    const tax_percent = parseInt(readLine(), 10);
+
+    solve(meal_cost, tip_percent, tax_percent);
+}

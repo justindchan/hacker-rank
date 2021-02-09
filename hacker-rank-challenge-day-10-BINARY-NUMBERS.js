300a1aa584b3fb895ae0b6633cd61e766b7e45e7
Justin <justindchan@gmail.com>
2021-02-09 14:30:47 -0800

    function that converts an integer from ase ten to base two.

---
+++ hacker-rank-challenge-day-10-BINARY-NUMBERS.js
@@ -0,0 +1,54 @@
+// Task
+// Given a base 10 integer, convert it to base 2.
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+function main() {
+    let n = parseInt(readLine(), 10);
+    var bin = "";
+    var currentValue;
+    var max = 0;
+    var count = 0;
+    
+    while (n > 0) {
+        currentValue = (n / 2) % 1 === 0 ? "0" : "1";
+        if(currentValue === "1") {
+            count++;
+        } else {
+            count = 0;
+        }
+        
+        if (count > max)  {
+            max = count;
+        }
+        
+        bin = currentValue + bin;
+        n = Math.floor(n/2);
+    }
+    
+    console.log(max);
+}
